023de07ecbae75ed50785d7b0268dacbfb54d39d
Ayush Kumar <devayush.me@gmail.com>
2025-05-22 18:30:05 +0530

    u

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Services.css"
 
-import Navbar from '../components/navbar/navbar'
+import Navbar from '../components/navbar/Navbar'
 import Location from '../components/location/Location'
 import Footer from '../components/footer/Footer'
 
